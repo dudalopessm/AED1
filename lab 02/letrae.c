75f80ec4f94167fee1d9a06ad1fc39641cf6ee9b
Eduarda Lopes <140003733+dudalopessm@users.noreply.github.com>
2024-01-18 23:54:54 -0300

    Problem in case e solved.

--- lab 02/letrae.c
+++ lab 02/letrae.c
@@ -15,19 +15,17 @@ void opcoes () {
 int main () {
     printf("\n\n######## Informacoes dos clientes ########");
     printf("\n\nInsira a quantidade de clientes que ira consultar: ");
-    int n, i;
+    int n, op, i, r, n2;
+    op = 0;
     setbuf(stdin, NULL);
     scanf("%d", &n);
     struct cliente c[n];
     if (c == NULL) return 0;  
     opcoes();
-    printf("\n\nA opcao desejada corresponde ao numero: ");
-    int op;
+    while (op != 4) {
+    printf("\nA opcao desejada corresponde ao numero: ");
     setbuf(stdin, NULL);
     scanf("%d", &op);
-    int r, n2;
-    r = 0;
-    while (op != 4) {
         if (op == 1) {
             printf("\n\nQuantos clientes deseja inserir: ");
             setbuf(stdin, NULL);
@@ -36,20 +34,21 @@ int main () {
                 printf("\n\nDigite o cpf do cliente a ser inserido: ");
                 setbuf(stdin, NULL);
                 scanf("%d", &c[i].cpf);
-                printf("\n\nDigite o estado civil do cliente a ser inserido: ");
+                printf("\nDigite o estado civil do cliente a ser inserido: ");
                 setbuf(stdin, NULL);
                 fgets(c[i].civil, 20, stdin);
-                printf("\n\nO cliente a ser inserido tem filhos? Se sim digite o numero, se nao digite 0: ");
+                printf("\nO cliente a ser inserido tem filhos? Se sim digite o numero, se nao digite 0: ");
                 setbuf(stdin, NULL);
                 scanf("%d", &c[i].filhos);
                 if (c[i].filhos > 0) {
-                    printf("\n\nInsira o nome dos filhos, separados por virgulas: ");
+                    printf("\nInsira o nome dos filhos, separados por virgulas: ");
                     setbuf(stdin, NULL);
                     fgets(c[i].nomesf, 50, stdin);
                     
                 }
             }
-            break;
+            printf("\n\nVoltando...");
+            printf("\n");
         } else if (op == 2) {
             printf("\n\nDigite a posicao no sistema do cliente deseja remover: ");
                 setbuf(stdin, NULL);
@@ -58,30 +57,26 @@ int main () {
                     c[i].cpf = c[i+1].cpf;
                 }
             n--;
-            break;
+            printf("\n\nVoltando...");
+            printf("\n");
         } else if (op == 3) {
             printf("\n\nClientes: ");
             printf("\n");
             for (i = 0; i < n; i++) {
                 if (c[i].filhos > 0) {
-                    printf("%d (pai ou mae de: %s)", c[i].cpf, c[i].nomesf);
+                    printf("\n - %d que possui parentesco com: %s", c[i].cpf, c[i].nomesf);
                 } else {
-                    printf("%d, ", c[i].cpf);
+                    printf("\n - %d", c[i].cpf);
                 }
             }
+            printf("\n\nVoltando...");
             printf("\n");
-            break;
-        }
-    opcoes();
-    printf("\n\nA opcao desejada corresponde ao numero: ");
-    setbuf(stdin, NULL);
-    scanf("%d", &op); 
-    }
-    if (op == 4) {
+        } else if (op == 4) {
         printf("\n\nEncerrando o software...");
         printf("\n\n######## Obrigada por usar! ########");
         printf("\n");
-        return 0;
+            break;
         }
+    }
     return 0;
 }
